1f37813965551b99610e5c8285e7407a7f1d65c7
rizwan <rizwan@uni-heidelberg.de>
2024-04-19 17:54:47 +0200

    refactor: add user type

--- src/stores/currentUser.ts
+++ src/stores/currentUser.ts
@@ -1,8 +1,14 @@
 import { defineStore } from 'pinia'
 import { useCurrentUser } from 'vuefire'
 
+export interface CurrentUserStore {
+  currentUser: any // TODO: need to be more precise
+}
+
 export const useCurrentUserStore = defineStore('currentUser', () => {
   const currentUser = useCurrentUser()
 
-  return { currentUser }
+  return {
+    currentUser,
+  } as CurrentUserStore
 })
