76588085c6f52c5bb73c9ccc7c9c24291ba8419d
barshathakuri <barshathakuri708@gmail.com>
2025-08-13 16:00:42 +0545

    chore(mapswipe-deployment): Update environment variables

--- web-app-serve/web-app-apply-config.sh
+++ web-app-serve/web-app-apply-config.sh
@@ -3,6 +3,7 @@
 set -xe
 
 while IFS='=' read -r KEY VALUE; do
-    # FIXME: If js and value is empty string, replace it with undefined?
-    find "$DESTINATION_DIRECTORY" -type f -exec sed -i "s|\<WEB_APP_SERVE_PLACEHOLDER__$KEY\>|$VALUE|g" {} +
+    REPLACEMENT="${VALUE:-undefined}"
+    find "$DESTINATION_DIRECTORY" -type f \
+        -exec sed -i "s|\<WEB_APP_SERVE_PLACEHOLDER__$KEY\>|$REPLACEMENT|g" {} +
 done < <(env | grep '^VITE_')
